c630511061cbb52273163f077327c3eb7e703b7b
umitcelebi <umitcelebi2035@gmail.com>
2023-08-24 23:17:30 +0300

    fraud module added

---
+++ fraud/src/main/java/com/umitclebi/fraud/FraudApplication.java
@@ -0,0 +1,16 @@
+package com.umitclebi.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * User: ucelebi
+ * Date: 24.08.2023
+ * Time: 22:13
+ */
+@SpringBootApplication
+public class FraudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
---
+++ fraud/src/main/java/com/umitclebi/fraud/FraudCheckHistory.java
@@ -0,0 +1,40 @@
+package com.umitclebi.fraud;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * User: ucelebi
+ * Date: 24.08.2023
+ * Time: 23:07
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FraudCheckHistory {
+
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Integer id;
+    private Integer customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
